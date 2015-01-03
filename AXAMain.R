7ba03d9ef9c2327f8b00e0eddeb5a0850b134b96
wacax <wacax@hotmail.com>
2015-01-03 01:32:47 -0500

    5 sigma outlier removal ready

--- AXAMain.R
+++ AXAMain.R
@@ -25,9 +25,11 @@ numCores <- detectCores()
 
 #Data Mining (Functions)------------------------
 #Transform data to distributions
-speedDistribution <- function(trip){
-  speed <- 3.6 * sqrt(diff(trip$x, 20, 1)^2 + diff(trip$y, 20, 1)^2) / 20
-  return(list(quantile(speed, seq(0.05, 1, by=0.05)), speed))
+speedDistribution <- function(trip, sigma = 5){
+  speed <-  3.6 * sqrt(diff(trip$x)^2 + diff(trip$y)^2) 
+  #Six sigma removal
+  speedWoOutliers <- speed[!speed > sd(speed) * sigma]  
+  return(list(quantile(speedWoOutliers, seq(0.05, 1, by=0.05)), speed))
 }
 
 #Define function to be passed as parallel
@@ -44,9 +46,9 @@ transform2Percentiles <- function(file, driverID){
 }
 
 #EDA----------------------------------------
-##Determine the minimal PCAs / number of neurons in the middle layer
+## EDA Pt. 1 Determine the minimal PCAs / number of neurons in the middle layer
 #Begin with a randomly selected driver to start the PCA calculation
-numberOfDrivers <- 200
+numberOfDrivers <- 300
 initialDrivers <- sample(drivers, numberOfDrivers)
 driversProcessed <- sapply(initialDrivers, function(driver){
   results <- unlist(mclapply(seq(1, 200), transform2Percentiles, mc.cores = numCores, driverID = driver))
@@ -61,7 +63,7 @@ driversProcessed <- scale(matrix(unlist(driversProcessed), nrow = numberOfDriver
 #Start h2o directly from R
 h2oServer <- h2o.init(ip = "localhost", max_mem_size = "5g", nthreads = -1) 
 
-h2oResult <- as.h2o(h2oServer, cbind(rep(c(0, 1, 1, 0), 50), driversProcessed)) 
+h2oResult <- as.h2o(h2oServer, driversProcessed) 
 print(h2o.ls(h2oServer))
 rm(driversProcessed)
 
@@ -72,10 +74,34 @@ plot(PCAModel@model$sdev)
 
 h2o.shutdown(h2oServer, prompt = FALSE)  
 
+##EDA Pt. 2 Visualization of Trajectories
+driverViz <- sample(drivers, 1)
+results <- mclapply(seq(1, 200), function(file, driverID){
+  tripCoordinates <- fread(file.path(driversDirectory, driverID, paste0(file, ".csv")))
+  return(cbind(tripCoordinates, rep(file, nrow(tripCoordinates))))
+}, mc.cores = numCores, driverID = driverViz)
+print(paste0("Driver number: ", driverViz, " processed"))
+
+dataTableReady2Plot <- do.call(rbind, results)
+qplot(x, y, data = dataTableReady2Plot, colour = V2, geom = "point")
+
+##EDA Pt. 3 Visualization of Speeds with and without outlier replacement
+driverViz <- sample(drivers, 1)
+fileViz <- sample(1:200, 1)
+
+tripCoordinates <- fread(file.path(driversDirectory, driverViz, paste0(fileViz, ".csv")))
+print(paste0("Driver number: ", driverViz, " trip number ", fileViz, " processed"))
+
+speed2Plot <-  3.6 * sqrt(diff(tripCoordinates$x)^2 + diff(tripCoordinates$y)^2)
+ggplot(as.data.frame(speed2Plot), aes(x = speed2Plot)) + geom_density()
+#Remove data above 5 sigmas (5 standard deviations)
+speed2Plot2 <- speed2Plot[!speed2Plot > sd(speed2Plot) * 5]
+ggplot(as.data.frame(speed2Plot2), aes(x = speed2Plot2)) + geom_density()
+
 #Unsupervised Learning and Hyperparameter Tuning--------------
 #Neural Network pre-training
 #Begin with a randomly selected driver to start the unsupervised learning
-numberOfDrivers <- 20
+numberOfDrivers <- 75
 initialDrivers <- sample(drivers, numberOfDrivers)
 driversProcessed <- sapply(initialDrivers, function(driver){
   results <- unlist(mclapply(seq(1, 200), transform2Percentiles, mc.cores = numCores, driverID = driver))
@@ -104,7 +130,7 @@ cvNNModel <- h2o.deeplearning(x = seq(2, ncol(h2oResult)), y = 1,
                               l2 = c(0, 1e-5),
                               rho = c(0.95, 0.99),
                               epsilon = c(1e-12, 1e-10, 1e-08),
-                              hidden = c(50, 25, 50), epochs = 50)
+                              hidden = c(55, 35, 55), epochs = 250)
 
 checkpointModel <- cvNNModel@model[[1]] #Best NN cv model 
 deepNetPath <- h2o.saveModel(object = checkpointModel, dir = file.path(workingDirectory), force = TRUE)
@@ -153,7 +179,7 @@ driversProb <- sapply(drivers, function(driver){
                                         l2 = optimall2,
                                         rho = optimalRho,
                                         epsilon = optimalEpsilon,
-                                        hidden = c(50, 25, 50), epochs = 500)
+                                        hidden = c(55, 35, 55), epochs = 500)
   
   anomalousTrips <- as.data.frame(h2o.anomaly(h2oResult, driverDeepNNModel))
   
