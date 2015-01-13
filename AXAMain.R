2238b47e857f3d5852b282923f8d1bb374660f2a
wacax <wacax@hotmail.com>
2015-01-13 00:44:40 -0500

    multiple instance learning and LOF algorithms included

--- AXAMain.R
+++ AXAMain.R
@@ -8,6 +8,7 @@ rm(list=ls(all=TRUE))
 require("data.table")
 require("parallel")
 require("h2o")
+require("DMwR")
 require("ggplot2")
 require("plotly")
 
@@ -23,12 +24,22 @@ drivers <- list.files(driversDirectory)
 numCores <- detectCores()
 
 #Data Mining (Functions)------------------------
+#Outlier Removal and transformation to quantiles
+quantSigma <- function(vector, sigma = 5, returnVector = TRUE){
+  #n-sigma removal
+  vectorWithoutOutliers <- vector[!vector > mean(vector) + sd(vector) * sigma]
+  if (returnVector == TRUE){
+    return(list(quantile(vectorWithoutOutliers, seq(0.05, 1, by=0.05)), vectorWithoutOutliers))
+  }else{
+    return(quantile(vectorWithoutOutliers, seq(0.05, 1, by=0.05)))    
+  }
+}
 #Transform data to speed distributions
-speedDistribution <- function(trip, sigma = 4){
+speedDistribution <- function(trip){
   speed <- 3.6 * sqrt(diff(trip$x)^2 + diff(trip$y)^2)
   #n-sigma removal
-  speedWoOutliers <- speed[!speed > mean(speed) + sd(speed) * sigma]
-  return(list(quantile(speedWoOutliers, seq(0.05, 1, by=0.05)), speedWoOutliers))
+  speedList <- quantSigma(speed)
+  return(speedList)
 }
 #Angles Mining with Angle Rotation
 angleVector <- function(dir, sigma = 4){
@@ -45,22 +56,24 @@ angleVector <- function(dir, sigma = 4){
   #set orthogonal values to zero
   angles[angles >= 90] <- 0  
   #Outliers removal
-  anglesWoOutliers <- angles[!angles > mean(angles) + sd(angles) * sigma]
-  return(list(quantile(anglesWoOutliers, seq(0.05, 1, by=0.05)), angles))
+  anglesWoOutliers <- quantSigma(angles, sigma = 4)
+  return(anglesWoOutliers)
 }
 #Define function to be passed as parallel
 transform2Percentiles <- function(file, driverID){
   trip <- fread(file.path(driversDirectory, driverID, paste0(file, ".csv")))
+  #Velocities
   speedData <- speedDistribution(trip)
   speedDist <- speedData[[1]]
   accelerations <- diff(speedData[[2]])
-  #Acceleration outlier removal
-  accelerationsWoOutliers <- accelerations[!accelerations > mean(accelerations) + sd(accelerations) * 5]  
-  accelerationDist <- quantile(accelerationsWoOutliers, seq(0.05, 1, by=0.05))
+  #Accelerations
+  accelerationDist <- quantSigma(accelerations, sigma = 4,  returnVector = FALSE)
+  #Distances
   distances <- sqrt((diff(trip$x)^2) + (diff(trip$y)^2))
   distances[distances > mean(distances) + sd(distances) * 5] <- NA
   distances[is.na(distances)] <- mean(distances, na.rm = TRUE)
   distanceTrip <- sum(distances)
+  #Angles
   anglesData <- angleVector(trip)
   turningAnglesDist <- anglesData[[1]]
   #anglesPlusSpeedist <- anglesData[[2]] * speedData[[2]][-1] #this doesn't work on the 1004th driver trip number 11
@@ -143,6 +156,15 @@ anglesWoOutliers[anglesWoOutliers >= 90] <- 0
 anglesWoOutliers <- anglesWoOutliers[!anglesWoOutliers > mean(anglesWoOutliers) + sd(anglesWoOutliers) * 4]
 ggplot(as.data.frame(anglesWoOutliers), aes(x = anglesWoOutliers)) + geom_density()
 
+##EDA Pt. 5 LOF Algorithm visualization
+driverViz <- sample(drivers, 1)
+results <- unlist(mclapply(seq(1, 200), transform2Percentiles, mc.cores = numCores, driverID = driverViz))
+results <- scale(matrix(results, nrow = 200, byrow = TRUE))
+#Plot
+par(mfrow=c(1, 2))
+plot(density(lofactor(results, k=5)))
+biplot(prcomp(results), cex=.8)
+
 #Unsupervised Learning and Hyperparameter Tuning--------------
 #Neural Network pre-training
 #Begin with a randomly selected driver to start the unsupervised learning
@@ -176,12 +198,6 @@ cvNNModel <- h2o.deeplearning(x = seq(2, ncol(h2oResult)), y = 1,
                               hidden = c(50, 40, 50), epochs = 50)
 checkpointModel <- cvNNModel@model[[1]] #Best NN cv model
 deepNetPath <- h2o.saveModel(object = checkpointModel, dir = file.path(workingDirectory), force = TRUE)
-optimalActivation <- cvNNModel@model[[1]]@model$params$activation
-optimalIDR <- cvNNModel@model[[1]]@model$params$input_dropout_ratio
-optimall1 <- cvNNModel@model[[1]]@model$params$l1
-optimall2 <- cvNNModel@model[[1]]@model$params$l2
-optimalRho <- cvNNModel@model[[1]]@model$params$rho
-optimalEpsilon <- cvNNModel@model[[1]]@model$params$epsilon
 h2o.shutdown(h2oServer, prompt = FALSE)
 
 #Modelling---------------------------
@@ -194,49 +210,88 @@ h2oServer <- h2o.init(ip = "localhost", max_mem_size = "5g", nthreads = -1)
 checkpointModel <- h2o.loadModel(h2oServer, deepNetPath)
 print(h2o.ls(h2oServer))
 checkpointModelKey <- h2o.ls(h2oServer)[, 1]
+
 driversProb <- sapply(drivers, function(driver){
   #Parallel processing of each driver data
   results <- unlist(mclapply(seq(1, 200), transform2Percentiles, mc.cores = numCores, driverID = driver))
   results <- scale(matrix(results, nrow = 200, byrow = TRUE))
-  #KSVMmodel <- ksvm(results, type = "one-svc", kernel="rbfdot", kpar="automatic", prob.model = TRUE)
-  #KSVMPrediction <- predict(KSVMmodel, results, type = "response")
-  #Plotting
-  #par(mfrow=c(1, 2))
-  #plot(density(lofactor(results, k=5)))
-  #biplot(prcomp(results), cex=.8)
+  print(paste0("Driver number ", driver, " processed"))
+  
+  #Sample data from other drivers  
+  numberOfDrivers <- 10
+  initialDrivers <- sample(drivers[!drivers %in% driver], numberOfDrivers)
+  ExtraDrivers <- sapply(initialDrivers, function(driver){
+    results <- unlist(mclapply(seq(1, 200), transform2Percentiles, mc.cores = numCores, driverID = driver))
+    print(paste0("Driver number: ", driver, " processed"))
+    return(results)
+  })
+  ExtraDrivers <- scale(matrix(unlist(ExtraDrivers), nrow = numberOfDrivers * 200, byrow = TRUE))  
+  
+  #LOF Algorithm
+  lofDriver <- lofactor(results, k=5)
+  lofDriverRanking <- rank(-lofDriver)
+  
+  #h20.ai deep learning
   #R matrix conversion to h2o object and stored in the server
-  h2oResult <- as.h2o(h2oServer, cbind(rep(c(0, 1, 1, 0), 50), results))
+  h2oResult <- as.h2o(h2oServer, cbind(c(rep(1, nrow(results)), rep(0, nrow(ExtraDrivers))), 
+                                       rbind(results, ExtraDrivers)))
+  h2oOnlyDriverTrips <- h2oResult[1:200, ]
+  h2oResult <- h2o.splitFrame(h2oResult, ratios = 1, shuffle = TRUE)[[1]]
+  
   print(h2o.ls(h2oServer))
   driverDeepNNModel <- h2o.deeplearning(x = seq(2, ncol(h2oResult)), y = 1,
                                         activation = optimalActivation,
-                                        data = h2oResult, autoencoder = TRUE,
+                                        data = h2oResult, 
                                         checkpoint = checkpointModel,
-                                        input_dropout_ratio = optimalIDR,
-                                        l1 = optimall1,
-                                        l2 = optimall2,
-                                        rho = optimalRho,
-                                        epsilon = optimalEpsilon,
+                                        input_dropout_ratio = c(0, 1),
+                                        l1 = c(0, 1e-5),
+                                        l2 = c(0, 1e-5),
+                                        rho = c(0.95, 0.99),
+                                        epsilon = c(1e-12, 1e-10, 1e-08),
                                         hidden = c(50, 40, 50), epochs = 500)
-  anomalousTrips <- as.data.frame(h2o.anomaly(h2oResult, driverDeepNNModel))
+  
+  #anomalousTrips <- as.data.frame(h2o.anomaly(h2oResult, driverDeepNNModel))
+  
   #MSE error transformation into pseudo-probabilities / chi-squared probability calculation
   #anomalousTrips[, 1] <- pchisq(anomalousTrips[, 1], df = 1)
+  
+  #probability Prediction of trips in driver #N
+  predictionNN <- signif(as.data.frame(h2o.predict(DeepNNModel, newdata = h2oOnlyDriverTrips)), digits = 4)
+  predictionNNRank <- rank(predictionNN[, 1])
+    
   print(h2o.ls(h2oServer))
   h2oObjects2Remove <- which(!h2o.ls(h2oServer)[, 1] %in% checkpointModelKey)
-  h2o.rm(object = h2oServer, keys = h2o.ls(h2oServer)[h2oObjects2Remove, 1])
-  print(paste0("Driver number ", driver, " processed"))
+  h2o.rm(object = h2oServer, keys = h2o.ls(h2oServer)[h2oObjects2Remove, 1])  
   print(paste0(which(drivers == driver), "/", length(drivers)))
-  return(anomalousTrips[, 1])
+  
+  return(cbind(predictionNN[, 1], predictionNNRank, lofDriver, lofDriverRanking))
 })
 h2o.shutdown(h2oServer, prompt = FALSE)
 
-#MSE error transformation into positive pseudo-probabilities-------------------
-#chi-squared probability calculation
-driversProb <- pchisq(driversProb, df = 1)
-driversProb <- 1 - as.vector(driversProb)
+deepNNProb <- 
+deepNNRank <- 
+lofScore <- 
+lofRank <- 
 
-#Write .csv-------------------------
+#Write .csv files-------------------------
 submissionTemplate <- fread(file.path(otherDataDirectory, "sampleSubmission.csv"), header = TRUE,
                             stringsAsFactors = FALSE, colClasses = c("character", "numeric"))
-submissionTemplate$prob <- signif(driversProb, digits = 5)
-write.csv(submissionTemplate, file = "SpeedNNMSEPredictionVI.csv", row.names = FALSE)
-system('zip SpeedNNMSEPredictionVI.zip SpeedNNMSEPredictionVI.csv')
+#Probabilities h2o.ai Deep NN
+submissionTemplate$prob <- signif(deepNNProb, digits = 5)
+write.csv(submissionTemplate, file = "deepNNProbI.csv", row.names = FALSE)
+system('zip deepNNProbI.zip deepNNProbI.csv')
+
+#Probabilities h2o.ai Deep NN
+submissionTemplate$prob <- signif(deepNNRank, digits = 5)
+write.csv(submissionTemplate, file = "deepNNRankI.csv", row.names = FALSE)
+system('zip deepNNRankI.zip SdeepNNRankI.csv')
+
+#Probabilities h2o.ai Deep NN
+submissionTemplate$prob <- signif(lofScore, digits = 5)
+write.csv(submissionTemplate, file = "lofScoreI.csv", row.names = FALSE)
+system('zip lofScoreI.zip lofScoreI.csv')
+
+#Probabilities h2o.ai Deep NN
+submissionTemplate$prob <- signif(lofRank, digits = 5)
+write.csv(submissionTemplate, file = "lofRankI.csv", row.names = FALSE)
+system('zip lofRankI.zip lofRankI.csv')
